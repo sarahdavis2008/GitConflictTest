5c10bcf17eb2d9e17abbf7f2e7a3298e7bf8e205
sarahdavis2008 <sdavis2@nic.edu>
2025-10-24 03:21:03 +0000

    added say function

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 
 using namespace std;
+void say(string word);
 
 int main()
 {
     cout << "My name is Sarah!" << endl;
     cout << "Hello World!" << endl;
-    
+    say("Hi");
     return 0;
+}
+
+void say (string word)
+{
+    cout << word << endl;
 }
