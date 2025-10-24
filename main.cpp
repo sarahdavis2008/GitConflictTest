092e1420cf3a4da313ccff1af6e2d07c36e1087a
SARAH DAVIS <sdavis2@nic.edu>
2025-10-23 20:09:56 -0700

    Added the add function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
 using namespace std;
+int add(int a, int b);
 
 int main()
 {
     cout << "Hello World!" << endl;
+    cout << "5 + 7 = " << add(5, 7) << endl;
     return 0;
+}
+
+int add(int a, int b)
+{
+    return a + b;
 }
